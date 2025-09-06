f24a0b5174181cdf450cd33acf5c47070b4fff5b
Nicolas <nicolas.barbosa@orange.fr>
2025-09-06 15:40:31 +0200

    Initial Commit

---
+++ Invoicer/InvoicerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoicerApp.swift
+//  Invoicer
+//
+//  Created by Nicolas Barbosa on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct InvoicerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
